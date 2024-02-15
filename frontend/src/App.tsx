0542070785e8570da6a3c3f36fa0dfbffb3ec482
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-02-15 18:03:16 +0530

    Delete unnecessary files and update dependencies
    Co-Authored-By: swatikamovaliya <160128764+swatikamovaliya@users.noreply.github.com>

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { SignOutButton, SignInButton, SignedIn, SignedOut } from "@clerk/clerk-react"
+
+function App() {
+ 
+  return (
+    <div>
+      <SignedOut>
+        <SignInButton mode={"modal"} />
+        <p>This content is public. Only signed out users can see the SignInButton above this text.</p>
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton  />
+        <p>This content is private. Only signed in users can see the SignOutButton above this text.</p>
+      </SignedIn>
+    </div>
+  )
+}
+ 
+export default App
