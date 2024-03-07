a9cba4f2d4c246eccd8a04708970511edbfed47e
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-03-07 17:29:20 +0530

    add pages

---
+++ frontend/src/pages/Facilities.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+const Facilities = () => {
+  return (
+    <div>
+      <Navbar />
+      <div>Facilities page</div>
+    </div>
+  );
+};
+
+export default Facilities;
