40278cd6fa5bcb5c5badef8e9e4f8b53a2ae082d
swatikamovaliya <swatikamovaliya@gmail.com>
2024-03-09 16:45:59 +0530

    update

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -4,10 +4,22 @@ const About = () => {
   return (
     <div>
       <Navbar />
-      <div>about page</div>
+      
+      <div className="grid h-screen bg-[#222222]">
+        <div>
+        <h1 className="">Welcome To CAPPA.</h1>
+        </div>
+
+        <div className="text-[#919191]">
+        <p className="mt-10">  Built in 2010 during the Belle Epoque period, this hotel is located in the center of India, with easy access to the city’s tourist attractions. It offers tastefully decorated rooms.</p>
+        </div>
+
+        <div className="grid">
+        <img src="https://themewagon.github.io/sogo/images/img_1.jpg" className="m-5 mt-20 h-96" alt="img" />
+        </div>
+      </div>
     </div>
   );
 };
 
 export default About;
-    
